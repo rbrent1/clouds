dfb57a183e5eba93c0119cb406a2ef646315e86a
rbrent1 <rbrent1@engr-134-12.eastern.ewu.edu>
2015-03-09 13:30:32 -0700

    Base Commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+
+$('#sky').jQlouds({
+  wind: true
+});
+
+$('#sky').jQlouds({
+  minClouds: 5,
+  maxClouds: 10
+});
+
+$('#sky').jQlouds({
+  maxWidth: 300,
+  maxHeight: 200,
+});
+
+$('#sky').jQlouds({
+  src: 'img/new-cloud.png',
+});
+
+
+
